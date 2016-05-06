a100f835836c9075ec9a7cce8dcac18e4769edde
hooni_win <kshmarine@gmail.com>
2016-05-06 13:49:20 +0900

    종료, 정보 구현

--- NotepadGUI.java
+++ NotepadGUI.java
@@ -1,4 +1,170 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class NotepadGUI {
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
+import javax.swing.border.EmptyBorder;
 
-}
+public class NotepadGUI extends JFrame implements ActionListener{
+
+	private JPanel contentPane;
+	private JScrollPane scrollPane;
+	private JTextArea ta_text;
+	private JMenuBar menuBar;
+	private JMenu menu_file;
+	private JMenuItem mi_file_new;
+	private JMenuItem mi_file_open;
+	private JMenuItem mi_file_save;
+	private JMenuItem mi_file_saveAs;
+	private JSeparator separator;
+	private JMenuItem mi_file_page;
+	private JMenuItem mi_file_print;
+	private JSeparator separator_1;
+	private JMenuItem mi_file_exit;
+	private JMenu menu_help;
+	private JMenuItem mi_help_help;
+	private JSeparator separator_2;
+	private JMenuItem mi_help_info;
+	private NotepadManager mgr = new NotepadManager();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NotepadGUI frame = new NotepadGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public NotepadGUI() {
+		setTitle("\uC81C\uBAA9\uC740 \uAC70\uB4E4\uBFD0 - \uD6C4\uB2C8\uBA54\uBAA8\uC7A5");
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				mi_file_exit.doClick();
+			}
+		});
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		menu_file = new JMenu("\uD30C\uC77C(F)");
+		menu_file.setMnemonic('f');
+		menuBar.add(menu_file);
+		
+		mi_file_new = new JMenuItem("\uC0C8\uB85C \uB9CC\uB4E4\uAE30(N)");
+		mi_file_new.addActionListener(this);
+		mi_file_new.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_MASK));
+		mi_file_new.setName("");
+		mi_file_new.setMnemonic(KeyEvent.VK_N);
+		menu_file.add(mi_file_new);
+		
+		mi_file_open = new JMenuItem("\uC5F4\uAE30(O)...");
+		mi_file_open.addActionListener(this);
+		mi_file_open.setMnemonic('o');
+		mi_file_open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
+		menu_file.add(mi_file_open);
+		
+		mi_file_save = new JMenuItem("\uC800\uC7A5(S)");
+		mi_file_save.addActionListener(this);
+		mi_file_save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+		mi_file_save.setMnemonic('s');
+		menu_file.add(mi_file_save);
+		
+		mi_file_saveAs = new JMenuItem("\uB2E4\uB978 \uC774\uB984\uC73C\uB85C \uC800\uC7A5(A)...");
+		mi_file_saveAs.addActionListener(this);
+		mi_file_saveAs.setMnemonic('a');
+		menu_file.add(mi_file_saveAs);
+		
+		separator = new JSeparator();
+		menu_file.add(separator);
+		
+		mi_file_page = new JMenuItem("\uD398\uC774\uC9C0 \uC124\uC815(U)...");
+		mi_file_page.addActionListener(this);
+		mi_file_page.setMnemonic('u');
+		menu_file.add(mi_file_page);
+		
+		mi_file_print = new JMenuItem("\uC778\uC1C4(P)");
+		mi_file_print.addActionListener(this);
+		mi_file_print.setMnemonic('p');
+		mi_file_print.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, InputEvent.CTRL_MASK));
+		menu_file.add(mi_file_print);
+		
+		separator_1 = new JSeparator();
+		menu_file.add(separator_1);
+		
+		mi_file_exit = new JMenuItem("\uB05D\uB0B4\uAE30(X)");
+		mi_file_exit.addActionListener(this);
+		mi_file_exit.setMnemonic('x');
+		menu_file.add(mi_file_exit);
+		
+		menu_help = new JMenu("\uB3C4\uC6C0\uB9D0(H)");
+		menu_help.setMnemonic('h');
+		menuBar.add(menu_help);
+		
+		mi_help_help = new JMenuItem("\uB3C4\uC6C0\uB9D0 \uBCF4\uAE30(H)");
+		mi_help_help.addActionListener(this);
+		mi_help_help.setMnemonic('h');
+		menu_help.add(mi_help_help);
+		
+		separator_2 = new JSeparator();
+		menu_help.add(separator_2);
+		
+		mi_help_info = new JMenuItem("\uBA54\uBAA8\uC7A5 \uC815\uBCF4(A)");
+		mi_help_info.addActionListener(this);
+		mi_help_info.setMnemonic('a');
+		menu_help.add(mi_help_info);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		ta_text = new JTextArea();
+		scrollPane.setViewportView(ta_text);
+	}//constructor
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
+		if(source == mi_file_exit){
+			mgr.file_exit(ta_text.getText(), this);
+		}
+		
+		
+		
+		
+		if(source == mi_help_info){
+			mgr.mi_help_info(this);
+		}
+	}//actionPerformed()
+
+}//class
--- NotepadManager.java
+++ NotepadManager.java
@@ -1,4 +1,24 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class NotepadManager {
+	
+	public void file_exit(String text, JFrame frame){
+		if(!text.equals("")){
+			int a = JOptionPane.showConfirmDialog(frame, "변경 내용을 저장 하시겠습니까?", "종료", JOptionPane.YES_NO_CANCEL_OPTION);
+				if(a == JOptionPane.OK_OPTION){
+					System.exit(0);		//이부분에서 저장 시켜야 함.
+				}
+		}else {
+			System.exit(0);
+		}
+	}//file_exit()
+	
+	
+	
+	
+	public void mi_help_info(JFrame frame){
+		JOptionPane.showMessageDialog(frame, "후니 메모장\n제작시작: 2016.05.05.\n제작완료: 0000.00.00.", "후니 메모장 정보", JOptionPane.INFORMATION_MESSAGE);
+	}
 
-}
+}//class
